4733f83ff44f5fb190e897728c3d02ae9153b998
cagoya <2561694274@qq.com>
2025-05-24 21:47:15 +0800

    迁移头文件

---
+++ src/include/base/LightingPanel.h
@@ -0,0 +1,29 @@
+#ifndef LIGHTINGPANEL_H
+#define LIGHTINGPANEL_H
+
+#include "IImGuiPanel.h"
+#include "base/light.h"
+#include <glm/glm.hpp>     
+#include <glm/gtc/type_ptr.hpp> 
+// 前向声明 ImGuiIO，避免在头文件中包含 "imgui_internal.h" 或过多 ImGui 细节 
+
+class LightingPanel : public IImGuiPanel {
+public:
+    // 引用操作 main.cpp 里面的变量
+	AmbientLight& ambientLightRef;
+    SpotLight& spotLightRef;
+    PointLight& pointLightRef;
+    DirectionalLight& directionalLightRef;
+    LightingPanel(const char* name,ImGuiIO& io,SpotLight& spotlightref, AmbientLight& ambientlightref, PointLight& pointlightref, DirectionalLight
+        & directionalightref,Material& materialref) 
+        : ambientLightRef(ambientlightref),spotLightRef(spotlightref),
+        pointLightRef(pointlightref),directionalLightRef(directionalightref)
+    {
+        PanelName = name;
+        IsVisible = true;
+    }
+
+    void Draw() override;
+};
+
+#endif 
--- src/include/outside/roof.h
+++ src/include/outside/roof.h
@@ -17,6 +17,10 @@ public:
     void draw(Shader shader, const glm::mat4& modelMatrix) const;
 private:
     Square square_;
+    glm::vec3 ka_ = glm::vec3(0.2f, 0.02f, 0.02f);
+    glm::vec3 kd_ = glm::vec3(1.0f, 0.0f, 0.0f);
+    glm::vec3 ks_ = glm::vec3(0.5f, 0.5f, 0.5f);
+    float ns_ = 32.0f;
 };
 
 #endif // GABLE_ROOF_H

710f60edf6552e398be26a4bcfb23b76c53b4c66
cagoya <2561694274@qq.com>
2025-05-29 22:07:31 +0800

    清理目录

--- cmake-build-debug/external/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/hao/Desktop/CG_Project/external/glm/glm
-# Build directory: C:/Users/hao/Desktop/CG_Project/cmake-build-debug/external/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

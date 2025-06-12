b5727261cae854b84892227f40d5c5da292dbac9
cagoya <2561694274@qq.com>
2025-06-12 08:52:37 +0800

    fix:某些东西比如窗户不应该有阴影

--- src/outside/outside.cpp
+++ src/outside/outside.cpp
@@ -16,8 +16,6 @@ void Outside::setup()
 void Outside::draw(Shader& shader, const glm::mat4& modelMatrix, bool useTexture)
 {
     wall_.draw(shader, modelMatrix,  useTexture);
-    ground_.draw(shader, modelMatrix, useTexture);
-    path_.draw(shader, modelMatrix, useTexture);
     fence_.draw(shader, glm::mat4(1.0f), useTexture);
     roof_.draw(shader, modelMatrix, useTexture);
     door_.draw(shader, modelMatrix, useTexture);
@@ -25,5 +23,9 @@ void Outside::draw(Shader& shader, const glm::mat4& modelMatrix, bool useTexture
     stone_.draw(shader, glm::translate(glm::mat4(1.0f), glm::vec3(0.7f, 0.01f, 4.0f)), useTexture);
     tree_.draw(shader, glm::translate(glm::mat4(1.0f), glm::vec3(1.5f,0.0f,1.5f)), useTexture);
     tree_.draw(shader, glm::translate(glm::mat4(1.0f), glm::vec3(-1.5f, 0.0f, 1.5f)), useTexture);
-    window_.draw(shader, modelMatrix, useTexture);
+    if (useTexture) {
+        ground_.draw(shader, modelMatrix, useTexture);
+        path_.draw(shader, modelMatrix, useTexture);
+        window_.draw(shader, modelMatrix, useTexture);
+    }
 }
